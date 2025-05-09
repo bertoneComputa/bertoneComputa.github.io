a5f8683534c36451e1d5b873a40b1279864f18ae
bertoneComputa <138202104+bertoneComputa@users.noreply.github.com>
2025-05-09 18:30:15 +0000

    Added simplified HTML phonebook

---
+++ api/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+contacts = []
+
+@app.route('/api/contacts', methods=['GET'])
+def get_contacts():
+    return jsonify(contacts)
+
+@app.route('/api/contacts', methods=['POST'])
+def add_contact():
+    contact = request.get_json()
+    contacts.append(contact)
+    return jsonify(contact), 201
+
+@app.route('/api/contacts/<int:id>', methods=['PUT'])
+def update_contact(id):
+    contact = request.get_json()
+    contacts[id] = contact
+    return jsonify(contact)
+
+@app.route('/api/contacts/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    contact = contacts.pop(id)
+    return jsonify(contact)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
